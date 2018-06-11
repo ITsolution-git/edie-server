56ea4f4cb0176e96c90ee3bee444f5c084ba69ba
itsolution-git <itsolution0820@gmail.com>
2018-06-11 23:06:44 +0800

    Added device api

--- src/main/java/com/securegion/eddieui/api/im_webapi/DeviceApi.java
+++ src/main/java/com/securegion/eddieui/api/im_webapi/DeviceApi.java
@@ -1,5 +1,8 @@
 package com.securegion.eddieui.api.im_webapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.securegion.eddieui.hook.IMHook;
+import com.securegion.eddieui.model.Message;
 import com.securegion.eddieui.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +10,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DeviceApi {
-//    @GetMapping("/getHostname") Object getHostname(
-//            String iporhost, String user, String password,
-//            boolean isWindows, String collectorId, boolean noCred) {
-//        return deviceService.getHostname(iporhost, user, password, isWindows, collectorId, noCred);
-//    }
-//
-//    @GetMapping("/isAgentUp")
-//    Result<String> isAgentUp(String id) {
-//        return deviceService.isAgentUp(id);
-//    }
-//
-//    @GetMapping("/installAgent") Result<String> installAgent(String id, String collectorId) {
-//        return deviceService.installAgent(id, collectorId);
-//    }
+    @Autowired IMHook imHook;
+    @Autowired ObjectMapper mapper;
+
+    @GetMapping("/getHostname") Object getHostname(
+            String iporhost, String user, String password,
+            boolean isWindows, String collectorId, boolean noCred) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Device")
+                .method("getHostname")
+                .data(mapper.createObjectNode()
+                        .put("iporhost", iporhost)
+                        .put("user", user)
+                        .put("password", password)
+                        .put("isWindows", isWindows)
+                        .put("collectorId", collectorId)
+                        .put("noCred", noCred)
+                ).build(), Object.class);
+    }
+
+    @GetMapping("/isAgentUp")
+    Result<String> isAgentUp(String id) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Device")
+                .method("isAgentUp")
+                .data(mapper.createObjectNode()
+                        .put("id", id)
+                ).build(), Result.class);
+    }
+
+    @GetMapping("/installAgent") Result<String> installAgent(String id, String collectorId) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Device")
+                .method("installAgent")
+                .data(mapper.createObjectNode()
+                        .put("id", id)
+                        .put("collectorId", collectorId)
+                ).build(), Result.class);
+    }
 }
