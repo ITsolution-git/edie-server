da3e9798043f2ebe6ddaf1fec88ad3e587d7b8df
itsolution-git <itsolution0820@gmail.com>
2018-06-11 17:47:04 +0800

    Initialize

---
+++ src/main/java/com/securegion/eddieui/EddieuiApplication.java
@@ -0,0 +1,12 @@
+package com.securegion.eddieui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddieuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EddieuiApplication.class, args);
+    }
+}
