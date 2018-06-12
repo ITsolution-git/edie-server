34bd3a7e6ceb6248b33b039173a33c02ad54dbe5
itsolution-git <itsolution0820@gmail.com>
2018-06-12 11:34:07 +0800

    Added search api

---
+++ src/main/java/com/securegion/eddieui/api/im_webapi/SearchApi.java
@@ -0,0 +1,45 @@
+package com.securegion.eddieui.api.im_webapi;
+
+import com.securegion.eddieui.hook.IMHook;
+import com.securegion.eddieui.model.GenericQuery;
+import com.securegion.eddieui.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/search")
+@RestController
+public class SearchApi {
+    @Autowired IMHook imHook;
+
+    @GetMapping("/query") Object query(GenericQuery req) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Search")
+                .method("query")
+                .data(req)
+                .build(), Object.class);
+    }
+
+    @GetMapping("/getMapping") Object getMapping() {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Search")
+                .method("getMapping")
+                .build(), Object.class);
+    }
+
+    @GetMapping("/fields")
+    List<Map<String, Object>> fields(GenericQuery req) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Search")
+                .method("fields")
+                .data(req)
+                .build(), List.class);
+    }
+}
