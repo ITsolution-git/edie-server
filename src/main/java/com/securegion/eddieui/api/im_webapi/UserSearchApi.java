6132251e222ed556d95864b5d83be46b76cbc127
itsolution-git <itsolution0820@gmail.com>
2018-06-13 12:01:49 +0800

    Added api

---
+++ src/main/java/com/securegion/eddieui/api/im_webapi/UserSearchApi.java
@@ -0,0 +1,56 @@
+package com.securegion.eddieui.api.im_webapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.securegion.eddieui.hook.IMHook;
+import com.securegion.eddieui.model.Message;
+import com.securegion.eddieui.model.UserSearch;
+import com.securegion.eddieui.util.PageRequestUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/usersearch")
+@RestController
+public class UserSearchApi {
+    @Autowired IMHook imHook;
+    @Autowired ObjectMapper mapper;
+
+    @GetMapping
+    Object getAll(@PageableDefault Pageable pageable) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("UserSearch")
+                .method("getAll")
+                .data(mapper.createObjectNode().put("pageRequest", PageRequestUtil.serialize((PageRequest)pageable)))
+                .build(), Object.class);
+    }
+
+    @PostMapping
+    @PutMapping("/{id}")
+    Object save(@RequestBody UserSearch entity) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("UserSearch")
+                .method("save")
+                .data(entity)
+                .build(), Object.class);
+    }
+
+    @DeleteMapping("/{id}")
+    Object delete(@PathVariable("id") String id) {
+        Map<String, Object> data = new HashMap<String, Object>(){{
+            put("id", id);
+        }};
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("UserSearch")
+                .method("delete")
+                .data(data)
+                .build(), Object.class);
+    }
+}
