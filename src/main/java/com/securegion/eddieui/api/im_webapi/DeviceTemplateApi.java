5758506ab4a5c0bca7f875daabbce1b41ec30875
itsolution-git <itsolution0820@gmail.com>
2018-06-13 18:35:22 +0800

    Add apis

---
+++ src/main/java/com/securegion/eddieui/api/im_webapi/DeviceTemplateApi.java
@@ -0,0 +1,71 @@
+package com.securegion.eddieui.api.im_webapi;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.securegion.eddieui.hook.IMHook;
+import com.securegion.eddieui.model.Gauge;
+import com.securegion.eddieui.model.Message;
+import com.securegion.eddieui.util.PageRequestUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/devicetemplate")
+@RestController
+public class DeviceTemplateApi {
+    @Autowired IMHook imHook;
+    @Autowired ObjectMapper mapper;
+
+    @GetMapping
+    Object getAll(@PageableDefault Pageable pageable) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("DeviceTemplate")
+                .method("getAll")
+                .data(mapper.createObjectNode().put("pageRequest", PageRequestUtil.serialize((PageRequest)pageable)))
+                .build(), Object.class);
+    }
+
+    @PostMapping
+    @PutMapping("/{id}")
+    Object save(@RequestBody JsonNode entity) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("DeviceTemplate")
+                .method("save")
+                .data(entity)
+                .build(), Object.class);
+    }
+
+    @DeleteMapping("/{id}")
+    Object delete(@PathVariable("id") String id) {
+        Map<String, Object> data = new HashMap<String, Object>(){{
+            put("id", id);
+        }};
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("DeviceTemplate")
+                .method("delete")
+                .data(data)
+                .build(), Object.class);
+    }
+
+    @GetMapping("/search/findByTagsIn")
+    Object findByTagsIn(List<String> tag) {
+        Map<String, Object> data = new HashMap<String, Object>(){{
+            put("tag", tag);
+        }};
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("DeviceTemplate")
+                .method("findByTagsIn")
+                .data(data)
+                .build(), Object.class);
+    }
+}
