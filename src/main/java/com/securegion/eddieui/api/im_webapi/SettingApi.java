a7eb5d779e3f9cd7b4d91d4c45d66847bed596d3
itsolution-git <itsolution0820@gmail.com>
2018-06-13 19:15:20 +0800

    Added apis

--- src/main/java/com/securegion/eddieui/api/im_webapi/SettingApi.java
+++ src/main/java/com/securegion/eddieui/api/im_webapi/SettingApi.java
@@ -1,36 +1,64 @@
 package com.securegion.eddieui.api.im_webapi;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.securegion.eddieui.hook.IMHook;
 import com.securegion.eddieui.model.Event;
 import com.securegion.eddieui.model.Message;
 import com.securegion.eddieui.model.Settings;
+import com.securegion.eddieui.util.PageRequestUtil;
 import com.securegion.eddieui.util.ResponseUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @RequestMapping("/setting")
 @RestController
 public class SettingApi {
     @Autowired IMHook imHook;
+    @Autowired ObjectMapper mapper;
+
+    @GetMapping
+    Object getAll(@PageableDefault Pageable pageable) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Setting")
+                .method("getAll")
+                .data(mapper.createObjectNode().put("pageRequest", PageRequestUtil.serialize((PageRequest)pageable)))
+                .build(), Object.class);
+    }
 
     @PostMapping
-    Settings save(@RequestBody Settings entity, HttpServletResponse res) {
-        Settings saved = null;
-        try {
-            saved = imHook.sendMessageSync(Message.builder()
-                    .functionCategory("Internal")
-                    .subcategory("Setting")
-                    .method("save")
-                    .build(), Settings.class);
-        } catch (Exception e) {
-            log.error("Error", e);
-        }
-        return ResponseUtil.wrapResponse(saved, res);
+    @PutMapping("/{id}")
+    Object save(@RequestBody JsonNode entity) {
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Setting")
+                .method("save")
+                .data(entity)
+                .build(), Object.class);
+    }
+
+    @DeleteMapping("/{id}")
+    Object delete(@PathVariable("id") String id) {
+        Map<String, Object> data = new HashMap<String, Object>(){{
+            put("id", id);
+        }};
+        return imHook.sendMessageSync(Message.builder()
+                .functionCategory("Internal")
+                .subcategory("Setting")
+                .method("delete")
+                .data(data)
+                .build(), Object.class);
     }
 
     @GetMapping("/search/envvars")
@@ -38,7 +66,7 @@ public class SettingApi {
         return imHook.sendMessageSync(Message.builder()
                 .functionCategory("Internal")
                 .subcategory("Setting")
-                .method("getAll")
+                .method("envvars")
                 .build(), List.class);
     }
 
