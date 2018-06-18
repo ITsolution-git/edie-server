2e6c654cf07ca7910b1783064cf7061189b030d6
itsolution-git <itsolution0820@gmail.com>
2018-06-18 12:11:07 +0800

    Fixed shape api

--- src/main/java/com/securegion/eddieui/api/srflow_webapi/ShapeApi.java
+++ src/main/java/com/securegion/eddieui/api/srflow_webapi/ShapeApi.java
@@ -3,11 +3,13 @@ package com.securegion.eddieui.api.srflow_webapi;
 import com.securegion.eddieui.Const;
 import com.securegion.eddieui.hook.FlowHook;
 import com.securegion.eddieui.model.Message;
+import com.securegion.eddieui.util.ResponseUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 
 @Log4j2
@@ -16,7 +18,8 @@ public class ShapeApi {
     @Autowired FlowHook flowHook;
 
     @GetMapping("/getAllShapes")
-    Shape[] getAllShapes() {
+    Object getAllShapes(HttpServletResponse res) {
+        Object data = null;
         try {
             Message msg = Message.builder()
                     .type(Const.MSG_TYPE_FUNC)
@@ -24,11 +27,10 @@ public class ShapeApi {
                     .subcategory("Shape")
                     .method("getAll")
                     .build();
-            Shape[] res = flowHook.sendMessageSync(msg, Shape[].class);
-            return res;
+            data = flowHook.sendMessageSync(msg, Object.class);
         } catch (Exception e) {
             log.error("Error", e);
         }
-        return null;
+        return ResponseUtil.wrapResponse(data, res);
     }
 }
