5d7bf126e53df2cbec8aadeea0b471aa46ac4959
itsolution-git <itsolution0820@gmail.com>
2018-06-30 15:59:12 +0800

    Added log

--- src/main/java/com/securegion/eddieui/api/BasicApi.java
+++ src/main/java/com/securegion/eddieui/api/BasicApi.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicApi {
     @GetMapping("/version") String version() {
-        return "1.3";
+        return "1.4";
     }
 }
