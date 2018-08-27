2099d8e93d8059d26b814f32eb3922d9920cfa5f
itsolution-git <itsolution0820@gmail.com>
2018-08-27 20:27:37 +0800

    Fixed field rule

--- src/main/java/com/securegion/eddieui/model/GrokFieldRule.java
+++ src/main/java/com/securegion/eddieui/model/GrokFieldRule.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,4 +14,5 @@ import lombok.NoArgsConstructor;
 public class GrokFieldRule {
     private String rule;
     private String value;
+    private List<String> values;
 }
