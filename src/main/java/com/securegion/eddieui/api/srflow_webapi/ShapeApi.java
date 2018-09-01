5c3fd7194b7f888b930c487eeff37657168705a8
itsolution-git <itsolution0820@gmail.com>
2018-09-01 11:27:19 +0800

    Added shape api

--- src/main/java/com/securegion/eddieui/api/srflow_webapi/ShapeApi.java
+++ src/main/java/com/securegion/eddieui/api/srflow_webapi/ShapeApi.java
@@ -1,23 +1,24 @@
 package com.securegion.eddieui.api.srflow_webapi;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.securegion.eddieui.Const;
 import com.securegion.eddieui.hook.FlowHook;
 import com.securegion.eddieui.model.Message;
 import com.securegion.eddieui.util.ResponseUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
 
 @Log4j2
 @RestController
+@RequestMapping("/shape")
 public class ShapeApi {
     @Autowired FlowHook flowHook;
 
-    @GetMapping("/getAllShapes")
+    @GetMapping("/getAll")
     Object getAllShapes(HttpServletResponse res) {
         Object data = null;
         try {
@@ -32,5 +33,16 @@ public class ShapeApi {
             log.error("Error", e);
         }
         return ResponseUtil.wrapResponse(data, res);
+    }
+
+    @PostMapping
+    Object add(@RequestBody JsonNode entity, HttpServletResponse res) {
+        return ResponseUtil.wrapResponse(flowHook.sendMessageSync(Message.builder()
+                .type(Const.MSG_TYPE_FUNC)
+                .functionCategory("Flow")
+                .subcategory("Shape")
+                .method("add")
+                .data(entity)
+                .build(), Object.class), res);
     }
 }
