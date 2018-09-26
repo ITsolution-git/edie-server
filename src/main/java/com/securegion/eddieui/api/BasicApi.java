bcd15a3fbeb25ecba4798777a0bdd5cfedc67050
itsolution-git <itsolution0820@gmail.com>
2018-09-26 22:03:11 +0800

    Added process main

--- src/main/java/com/securegion/eddieui/api/BasicApi.java
+++ src/main/java/com/securegion/eddieui/api/BasicApi.java
@@ -1,11 +1,24 @@
 package com.securegion.eddieui.api;
 
+import com.securegion.eddieui.messaging.MessagesProcessingMain;
+import com.securegion.eddieui.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BasicApi {
+    @Autowired MessagesProcessingMain messagesProcessingMain;
     @GetMapping("/version") String version() {
         return "1.4";
+    }
+
+    @PostMapping("/onMessage")
+    public void onMessage(@RequestBody List<Message> messages) {
+        messagesProcessingMain.enqueue(messages);
     }
 }
---
+++ src/main/java/com/securegion/eddieui/messaging/MessagesProcessingMain.java
@@ -0,0 +1,18 @@
+package com.securegion.eddieui.messaging;
+
+import com.securegion.eddieui.model.Message;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Log4j2
+@Service
+public class MessagesProcessingMain {
+    private Queue<Message> incomingMessagesQueue = new ConcurrentLinkedQueue<>();
+    public void enqueue(List<Message> messages) {
+        incomingMessagesQueue.addAll(messages);
+    }
+}
