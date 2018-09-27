cf90a874b7656f03adfa7b4ca62cbf447ca4d0fb
itsolution-git <itsolution0820@gmail.com>
2018-09-27 15:14:26 +0800

    Fixed ui socket func

---
+++ src/main/java/com/securegion/eddieui/messaging/commands/socket/UISocketFunc.java
@@ -0,0 +1,32 @@
+package com.securegion.eddieui.messaging.commands.socket;
+
+import com.securegion.eddieui.messaging.commands.Command;
+import com.securegion.eddieui.model.Message;
+import com.securegion.eddieui.websocket.WebSocketConfig;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.function.Function;
+
+@Log4j2
+@Service
+public class UISocketFunc implements Command {
+    @Autowired WebSocketConfig webSocket;
+    @Override
+    public Object run(String method, Map<String, Object> params, Message message) {
+        switch (method) {
+            case "pushIncidents":
+                return null;
+            default:
+                log.error("Method not found: " + method);
+        }
+        return null;
+    }
+
+    Function<Object, String> pushIncidents = (data) -> {
+        webSocket.send("incidentupdate", data);
+        return "";
+    };
+}
