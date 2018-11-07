c878213f8563199c13667146ef0421ad351bd052
itsolution-git <itsolution0820@gmail.com>
2018-11-07 10:44:10 +0200

    tested onMessage bulk receive

---
+++ src/test/java/com/securegion/eddieui/conf/Config_Tests_Environment.java
@@ -0,0 +1,15 @@
+package com.securegion.eddieui.conf;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by SERGE on 11/10/2018.
+ */
+@WebAppConfiguration
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Config_Tests_Environment {
+}
